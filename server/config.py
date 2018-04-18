21fa2213ed31111eda1dbd0fadb0b87c3d03848d
Kyle Carretto <kcarretto@gmail.com>
2018-04-18 12:22:44 -0400

    Added docker compose

--- server/config.py
+++ server/config.py
@@ -1,11 +1,15 @@
 """
 This module contains configuration information for the server.
 """
-API_KEY_FILE = '/opt/arsenal-c2/.arsenal_key'
-TEAMSERVER_URI = 'http://qa.redteam-arsenal.com'
+import os
 
-SERVER_ADDRESS = None   # Set this to override the public lookup of the server IP.
+API_KEY_FILE = '/opt/arsenal-c2/.arsenal_key'
+TEAMSERVER_URI = os.environ.get('TEAMSERVER_URI', 'http://qa.redteam-arsenal.com')
+
+SERVER_ADDRESS = None
+                        # Set this to override the public lookup of the server IP.
                         #This is used to determine the initial URI that session is beaconing to.
 
-LOG_LEVEL = 'DEBUG'     # Set this to the desired log level of the server.
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
+                        # Set this to the desired log level of the server.
                         # Log levels: DEBUG, INFO, WARN, CRIT, FATAL
